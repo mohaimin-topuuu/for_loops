450781b5693f36b1f4a5dcd6e073edc3bf4ed808
Mohaimin Topu <abmtopu73@gmail.com>
2025-01-20 18:52:40 +0600

    for loops basic added

---
+++ for_basics.py
@@ -0,0 +1,13 @@
+#Loops are used for sequential traversal for traversing list, string, tuples etc.
+
+# nums = [ 1, 2, 3, 4, 5]
+
+# for var in nums : 
+#     print(var)
+
+
+# Range function in Python
+
+# Range function starts from 0 and increases by 1. 
+
+#range(start, stop, step)
